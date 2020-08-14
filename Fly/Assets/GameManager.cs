e710051ae50eccdb7e82c9a19d2ea99f70fd0a71
Sai Deekshith <msaideekshith@gmail.com>
2020-08-14 12:25:39 +0530

    level

---
+++ Fly/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject gameTitle,gameOverCanvas,replayCanvas;
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+    }
+    public void Start()
+    {
+        Time.timeScale = 1;
+    }
+    public void GameOver()
+    {
+        gameTitle.SetActive(true);
+        gameOverCanvas.SetActive(true);
+        replayCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Fly/Assets/RodSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodSpawner : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer = 0;
+    public GameObject rod;
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newrod  = Instantiate(rod);
+            newrod.transform.position = transform.position + new Vector3(0,Random.Range(-height,height),0);
+            Destroy(newrod,15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
